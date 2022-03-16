29eef9909b9854b3ed0832c8481474cbbdc58727
H3cth0r <hectorsucre13@gmail.com>
2022-03-15 23:12:49 -0600

    animation_advanements_sign_reg

--- website_sd/scripts/sign_reg.js
+++ website_sd/scripts/sign_reg.js
@@ -1,8 +1,60 @@
-
-
 // anime({
 //     targets: '#to_animate_button_one',
 //     translateY: -300,
 //     duration:   1000,
 //     easing: 'linear',
 // });
+
+
+let sign_up_up = false;
+let log_in_up  = false;
+
+
+
+function on_click_sign_up(){
+    if(log_in_up == true) log_in_down();
+    anime({
+        targets: '#sign_up_a',
+        translateY: -200,
+        duration: 1000,
+        easing: 'linear'
+    });
+    setTimeout(() => {
+        $(".sign_up_div_two").css("display", "flex");
+        $(".button_one").css("display", "block")
+        sign_up_down(1);
+        }, 1000);
+    
+    sign_up_up = true;
+}
+function sign_up_down(the_delay=1000){
+    anime({
+        targets: '#sign_up_a',
+        translateY: 0,
+        duration: the_delay,
+        easing: 'linear'
+    });   
+}
+
+
+
+
+function on_click_log_in(){
+    if(sign_up_up==true) sign_up_down();
+    anime({
+        targets: '#login_a',
+        translateY: -200,
+        duration: 1000,
+        easing: 'linear'
+    });
+    log_in_up = true;
+}
+
+function log_in_down(){
+    anime({
+        targets: '#login_a',
+        translateY: 0,
+        duration: 1000,
+        easing: 'linear'
+    });
+}
