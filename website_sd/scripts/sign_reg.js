0a44134680ac3e5156ce30e98540759fb9a4b047
H3cth0r <hectorsucre13@gmail.com>
2022-03-16 10:30:51 -0600

    sign_up_animation

--- website_sd/scripts/sign_reg.js
+++ website_sd/scripts/sign_reg.js
@@ -10,51 +10,60 @@ let sign_up_up = false;
 let log_in_up  = false;
 
 
+function translate_up_or_down(html_object, to_translate, the_duration = 1000, the_easing='linear'){
+    anime({targets: html_object, translateY: to_translate, duration: the_duration, easing: the_easing});
+}
+
+
 
 function on_click_sign_up(){
-    if(log_in_up == true) log_in_down();
-    anime({
-        targets: '#sign_up_a',
-        translateY: -200,
-        duration: 1000,
-        easing: 'linear'
-    });
+    // If it is already up exit function
+    if(sign_up_up == true) return;
+    // if the other login up div, move it down
+    if(log_in_up == true){ 
+        // 1. display form to none 
+        $(".log_in_div_two").css("display", "none");
+        $(".button_two").css("display", "none")
+        // 2. move "a" fast to -200
+        translate_up_or_down('#sign_up_a', -200, 1, 'linear');
+        // 3. animate down to 0
+        translate_up_or_down('#sign_up_a', 0, 1000, 'linear');
+        log_in_up = false;
+    }
+    // Move up the sign up button
+    translate_up_or_down('#sign_up_a', -200, 1000, 'linear');
     setTimeout(() => {
         $(".sign_up_div_two").css("display", "flex");
         $(".button_one").css("display", "block")
-        sign_up_down(1);
+        // move sign up a down fast, so it is leveled
+        translate_up_or_down('#sign_up_a', 0, 1, 'linear');
         }, 1000);
-    
     sign_up_up = true;
 }
-function sign_up_down(the_delay=1000){
-    anime({
-        targets: '#sign_up_a',
-        translateY: 0,
-        duration: the_delay,
-        easing: 'linear'
-    });   
-}
-
 
 
 
 function on_click_log_in(){
-    if(sign_up_up==true) sign_up_down();
-    anime({
-        targets: '#login_a',
-        translateY: -200,
-        duration: 1000,
-        easing: 'linear'
-    });
+    // If it is already up exit function
+    if(log_in_up == true) return;
+    // if the other login up div, move it down
+    if(sign_up_up == true){ 
+        // 1. display form to none 
+        $(".sign_up_div_two").css("display", "none");
+        $(".button_one").css("display", "none")
+        // 2. move "a" fast to -200
+        translate_up_or_down('#login_a', -200, 1, 'linear');
+        // 3. animate down to 0
+        translate_up_or_down('#login_a', 0, 1000, 'linear');
+        sign_up_up = false;
+    }
+    // Move up the sign up button
+    translate_up_or_down('#login_a', -200, 1000, 'linear');
+    setTimeout(() => {
+        $(".log_in_div_two").css("display", "flex");
+        $(".button_two").css("display", "block")
+        // move sign up a down fast, so it is leveled
+        translate_up_or_down('#login_a', 0, 1, 'linear');
+        }, 1000);
     log_in_up = true;
-}
-
-function log_in_down(){
-    anime({
-        targets: '#login_a',
-        translateY: 0,
-        duration: 1000,
-        easing: 'linear'
-    });
 }
