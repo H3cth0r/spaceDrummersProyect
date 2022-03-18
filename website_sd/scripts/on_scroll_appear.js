709f77b1ca7c6ecb22cc2ce3ce74a10e6eb198f2
H3cth0r <hectorsucre13@gmail.com>
2022-03-17 21:56:41 -0600

    animation fixes and stats animations

--- website_sd/scripts/on_scroll_appear.js
+++ website_sd/scripts/on_scroll_appear.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
     $(window).scroll( function(){
-        $('.general_site_div').each( function(i){           
+        $('.appear_class').each( function(i){           
             var bottom_of_element = $(this).offset().top + $(this).outerHeight();
             var bottom_of_window = $(window).scrollTop() + $(window).height();
             if( bottom_of_window > bottom_of_element ){
