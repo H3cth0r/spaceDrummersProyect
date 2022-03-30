d07200afd27d87d3eb23eb420e8ef403be8e5ca7
GermanTec <54922953+GermanWong@users.noreply.github.com>
2022-03-30 14:13:50 -0600

    gamesesion german
    agregacion de datos exitosa

--- django_server/sd_server/videojuego/views.py
+++ django_server/sd_server/videojuego/views.py
@@ -1,6 +1,27 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+import sqlite3
+from json import dumps, loads
+import collections
 # Create your views here.
 
 def index(request):
     return HttpResponse('<h1> Hola desde Django</h1>')
+
+@csrf_exempt
+def gamesesion(request):
+    body_unicode = request.body.decode('utf-8')
+    body = loads(body_unicode)
+    startTime = body['startTime']
+    endTime = body['endTime']
+    userId = body['userId']
+    mydb = sqlite3.connect("db.sqlite3")
+    cur = mydb.cursor()
+    stringSQL = '''INSERT INTO "main"."gamesesion" ( "startTime", "endTime","userId") VALUES (?,?,?);'''
+    cur.execute(stringSQL, (startTime,endTime,userId,))
+    mydb.commit()
+
+    d = {"Perfecto":"Datos subidos"}
+
+    return JsonResponse(d, safe=False)
