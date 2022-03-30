475ae3004181d511170b1840ce6a9296ee9fbe2e
Victor P <victor32154@gmail.com>
2022-03-29 21:31:03 -0600

    initial branch commit :)

--- django_server/sd_server/videojuego/urls.py
+++ django_server/sd_server/videojuego/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('unityLogin', views.unityLogin, name='unityLogin')
 ]
--- django_server/sd_server/videojuego/views.py
+++ django_server/sd_server/videojuego/views.py
@@ -1,6 +1,47 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse, Http404
+from json import loads, dumps #para trabajar con json
+from django.views.decorators.csrf import csrf_exempt #para la directiva
+import sqlite3
+
 # Create your views here.
 
 def index(request):
     return HttpResponse('<h1> Hola desde Django</h1>')
+
+@csrf_exempt #directiva
+def unityLogin(request):
+    body_unicode = request.body.decode('utf-8')
+    body = loads(body_unicode)
+    username = body['username']
+    hashedPwdReq = body['hashedPwd']
+    mydb = sqlite3.connect("db.sqlite3")
+    cur = mydb.cursor()
+    stringSQL = '''SELECT id, hashedPwd FROM user WHERE id in (SELECT userId FROM gameprofile WHERE username=?)'''
+    row = cur.execute(stringSQL, (username,))
+    row = row.fetchone()
+    
+    if row != None:
+        userId = row[0]
+        hashedPwdDB = row[1]
+        accessGranted = 0
+        currentLevel=-1
+        
+        if hashedPwdReq == hashedPwdDB:
+            accessGranted = 1
+            stringSQL = '''SELECT currentLevel FROM gameprofile WHERE username=?'''
+            row = cur.execute(stringSQL, (username,))
+            row = row.fetchone()
+            currentLevel = row[0]
+        
+        d = {
+                "userId":userId,
+                "username":username,
+                "accessGranted":accessGranted,
+                "currentLevel":currentLevel
+                }
+
+    else:
+        raise Http404("username does not exist")
+
+    return JsonResponse(d, safe=False)
