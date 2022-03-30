7a2615b29815d2bd3f807d31e6942f38559f7872
Misaelchavez16 <75141227+Misaelchavez16@users.noreply.github.com>
2022-03-30 14:35:13 -0600

    Levelstats

--- django_server/sd_server/videojuego/views.py
+++ django_server/sd_server/videojuego/views.py
@@ -1,6 +1,31 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from json import loads, dumps
+import collections
+import sqlite3 
+from django.http import Http404
 # Create your views here.
 
 def index(request):
     return HttpResponse('<h1> Hola desde Django</h1>')
+
+@csrf_exempt
+def levelstats(request):
+    body_unicode = request.body.decode('utf-8')
+    body = loads(body_unicode)
+    levelId = body['levelId']
+    username = body['username']
+    score = body['score']
+    timeWhenScore = body['timeWhenScore']
+    kos = body['kos']
+    failedShoots = body['failedShoots']
+    mydb = sqlite3.connect("db.sqlite3")
+    cur = mydb.cursor()
+    stringSQL = '''INSERT INTO "main"."levelstats" ("levelId", "username","score", "timeWhenScore", "kos", "failedShoots") VALUES (?,?,?,?,?,?);'''
+    cur.execute(stringSQL, (levelId, username, score, timeWhenScore, kos, failedShoots))
+    mydb.commit()
+
+    d = {"Perfecto":"Datos subidos"}
+
+    return JsonResponse(d, safe=False)
