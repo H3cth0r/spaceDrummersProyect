bd96396a349de590fcd1f423eab0ffe1e684cb42
GermanTec <54922953+GermanWong@users.noreply.github.com>
2022-04-19 18:03:58 -0500

    Disminucion Nav bar stats

--- django_server/sd_server/static/scripts/navbar_animation.js
+++ django_server/sd_server/static/scripts/navbar_animation.js
@@ -7,19 +7,19 @@ function translate_left_or_right(html_object, to_translate, the_duration = 1000,
 function nav_bar_slide_left(){
     $('.clicked_sandwich_menu').css({'display': 'flex', 'background-color':'transparent'});
     $('.clicked_sandwich_menu nav ul li a').css({'color':'transparent'});
-    translate_left_or_right('.clicked_sandwich_menu', 1230, 500, 'linear');
+    translate_left_or_right('.clicked_sandwich_menu', 500, 500, 'linear');
     
     setTimeout(() => {
         $('.clicked_sandwich_menu').css({'background-color':'#D3450D'});
         $('.clicked_sandwich_menu nav ul li a').css({'color':'white'});
-        translate_left_or_right('.clicked_sandwich_menu', 0, 1500, 'linear');
+        translate_left_or_right('.clicked_sandwich_menu', 0, 1000, 'linear');
         }, 500);
     
     $('.close_clicked_sandwich_menu').css('display', 'block');
 }
 
 function nav_bar_slide_right(){
-    translate_left_or_right('.clicked_sandwich_menu', 1230, 1500, 'linear');
+    translate_left_or_right('.clicked_sandwich_menu', 500, 1000, 'linear');
     setTimeout(() => {
         $('.clicked_sandwich_menu').css({'display':'none'}); 
         $('.close_clicked_sandwich_menu').css('display', 'none');
