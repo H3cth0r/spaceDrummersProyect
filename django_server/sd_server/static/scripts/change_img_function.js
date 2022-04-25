f227a8112db8c242e0819ab1fa88b4b4bfde1bf9
H3cth0r <hectorsucre13@gmail.com>
2022-04-24 19:37:16 -0500

    post image as base64 to server

--- django_server/sd_server/static/scripts/change_img_function.js
+++ django_server/sd_server/static/scripts/change_img_function.js
@@ -6,13 +6,26 @@ $(function(){
     if (input.files && input.files[0]&& (ext == "gif" || ext == "png" || ext == "jpeg" || ext == "jpg")) 
      {
         var reader = new FileReader();
+        var the_result;
 
         reader.onload = function (e) {
-        //    $('#img').attr('src', e.target.result);
             $(".left_menu_img").css("background-image", 'url(' + e.target.result + ')');
-            console.log("changed img" + e.target.result);
-        }
-       reader.readAsDataURL(input.files[0]);
+            the_result = e.target.result;
+            console.log(the_result);
+            var petition = {"img_base" : String(the_result)};
+            $.ajax({
+                    type        :"POST",
+                    url         :"/takeThisPhoto",
+                    data        :JSON.stringify(petition),
+                    contentType :'application/json; charset=utf-8',
+                    success     :function(data){
+                      console.log("changed img == True");
+                    }
+            });
+          }
+
+        reader.readAsDataURL(input.files[0]);
+       
     }
     else
     {
